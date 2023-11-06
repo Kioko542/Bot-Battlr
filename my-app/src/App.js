17b6e75098592fa77895a46ed922d308da619389
Emmanue <kioxmanu7@gmail.com>
2023-11-06 09:50:09 +0300

    bot-card created

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,32 +1,12 @@
-// App.js
-import React, { Component } from 'react';
-import BotCollection from './components/BotCollection';
+import React, { Component } from "react";
+import BotsPage from "./containers/BotsPage";
+import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      allBots: [], // Initialize with an empty array
-    };
-  }
-
-  componentDidMount() {
-    fetch(' http://localhost:3000/bots') 
-      .then((response) => response.json())
-      .then((data) => {
-        // Update state with the fetched data
-        this.setState({ allBots: data });
-      })
-      .catch((error) => {
-        console.error('Error fetching data:', error);
-      });
-  }
-
   render() {
     return (
-      <div>
-        <h1>Bot Battlr</h1>
-        <BotCollection bots={this.state.allBots} />
+      <div className="App">
+        <BotsPage />
       </div>
     );
   }
--- my-app/src/components/BotCard.js
+++ my-app/src/components/BotCard.js
@@ -1,20 +1,62 @@
-// BotCard.js
-import React from 'react';
+import React from "react";
 
-function BotCard({ bot, onEnlist }) {
+const BotCard = props => {
+  const { bot } = props;
+
+  let botType;
+
+  switch (bot.bot_class) {
+    case "Assault":
+      botType = <i className="icon military" />;
+      break;
+    case "Defender":
+      botType = <i className="icon shield" />;
+      break;
+    case "Support":
+      botType = <i className="icon ambulance" />;
+      break;
+    default:
+      botType = <div />;
+  }
+
   return (
-    <div className="bot-card">
-      <img src={bot.avatar_url} alt={bot.name} />
-      <h3>{bot.name}</h3>
-      <div className="par">
-      <p>Health: {bot.health}</p>
-      <p>Damage: {bot.damage}</p>
-      <p>Armor: {bot.armor}</p>
-      <p>Class: {bot.bot_class}</p>
+    <div className="ui column">
+      <div
+        className="ui card"
+        key={bot.id}
+        onClick={() => props.addBot(bot)}
+      >
+        <div className="image">
+          <img alt="oh no!" src={bot.avatar_url} />
+        </div>
+        <div className="content">
+          <div className="header">
+            {bot.name} {botType}
+          </div>
+
+          <div className="meta text-wrap">
+            <small>{bot.catchphrase}</small>
+          </div>
+        </div>
+        <div className="extra content">
+          <span>
+            <i className="icon heartbeat" />
+            {bot.health}
+          </span>
+
+          <span>
+            <i className="icon lightning" />
+            {bot.damage}
+          </span>
+          <span>
+            <i className="icon shield" />
+            {bot.armor}
+          </span>
+        </div>
       </div>
-      <button onClick={onEnlist}>Enlist</button>
     </div>
   );
-}
+
+};
 
 export default BotCard;
